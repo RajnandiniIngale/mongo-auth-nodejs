7c037a87058133af3a9422a8769355f7f07cd4dc
Rajnandini Ingale <rajnandini.ingale@anthaathi.co>
2024-09-24 16:42:41 +0530

    feat: changes

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
-const express = require('express');
-
+const express = require("express");
+const User = require("./models/user");
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.json());
 
-app.listen(PORT, ()=> {
-    console.log(`Server listening on http://localhost:${PORT}`)
-})
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: "Email already registered" });
+    }
+
+    const newUser = new User({ email, password });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    console.error("Error registering User: ", err);
+
+    res.status(500).json({ message: "Error occurred while registering User" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ error: "Invalid Email or Password" });
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+
+    const token = jwt
+  } catch (err) {}
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const brcypt = require("bcrypt");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const hashedPassword = await brcypt.hash(password, 10);
+
+    const user = new User({ username, password: hashedPassword });
+
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Registration failed" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ error: "Authentication failed" });
+    }
+
+    const passwordMatch = await brcypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Authentication failed" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, "raj-secret-key", {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({ error: "Login failed" });
+  }
+});
+
+
+module.exports = router;
